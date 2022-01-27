db1def0878cc8fdc4196712fad9c48dd32b3b795
darkvpier <shraidshebin@gmail.com>
2022-01-27 14:58:33 +0530

    fixed Symlink

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@
 ln -sf $PWD/.package/.tmux.conf ~/.tmux.conf
 
 if [ -d "$HOME/.config/micro" ]; then
-	ln -sf $PWD/.package/micro/syntax ~/.config/micro/syntax
-	ln -sf $PWD/.package/micro/bindings.json ~/.config/micro/bindings.json
+	rm -rf $HOME/.config/micro/syntax
+	ln -sf $PWD/.package/micro/syntax $HOME/.config/micro/syntax
+	ln -sf $PWD/.package/micro/bindings.json $HOME/.config/micro/bindings.json
 else
 	mkdir ~/.config/micro
-	ln -sf $PWD/.package/micro/syntax ~/.config/micro/
-	ln -sf $PWD/.package/micro/bindings.json ~/.config/micro/bindings.json
+	ln -sf $PWD/.package/micro/syntax $HOME/.config/micro/
+	ln -sf $PWD/.package/micro/bindings.json $HOME/.config/micro/bindings.json
 fi
