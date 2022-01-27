bb6ead5c8d96a972d7e1977234f79ad21103a0ca
darkvpier <shraidshebin@gmail.com>
2022-01-27 03:39:19 -0500

    Added installation Package

--- install.sh
+++ install.sh
@@ -1,1 +1,12 @@
 #/bin/bash
+
+ln -sf $PWD/.package/.tmux.conf ~/.tmux.conf
+
+if [ -d "$HOME/.config/micro" ]; then
+	ln -sf $PWD/.package/micro/syntax ~/.config/micro/syntax
+	ln -sf $PWD/.package/micro/bindings.json ~/.config/micro/bindings.json
+else
+	mkdir ~/.config/micro
+	ln -sf $PWD/.package/micro/syntax ~/.config/micro/syntax
+	ln -sf $PWD/.package/micro/bindings.json ~/.config/micro/bindings.json
+fi
