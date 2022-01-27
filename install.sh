7be92551f51ea2006eb3bc0c6767f778abd8335a
darkvpier <shraidshebin@gmail.com>
2022-01-27 03:57:37 -0500

    fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 ln -sf $PWD/.package/.tmux.conf ~/.tmux.conf
 
 if [ -d "$HOME/.config/micro" ]; then
-	ln -sf $PWD/.package/micro/syntax ~/.config/micro/
+	ln -sf $PWD/.package/micro/syntax ~/.config/micro/syntax
 	ln -sf $PWD/.package/micro/bindings.json ~/.config/micro/bindings.json
 else
 	mkdir ~/.config/micro
