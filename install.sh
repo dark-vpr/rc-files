ae766b8eb7fae76e0031464d1fbcef99a157be5f
darkvpier <shraidshebin@gmail.com>
2022-06-11 03:16:32 +0530

    fixed installation of Spacevim

--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #/bin/bash
 
-ln -sf $PWD/.package/.tmux.conf ~/.tmux.conf
+ln -sf $PWD/.package/.$HOME/.SpaceVim.dtmux.conf ~/.tmux.conf
 if [ -d $HOME/.tmux/plugins/tpm ]; then
-	printf "TPM has already been Installed.....\n"
+  printf "TPM has already been Installed.....\n"
 else
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	echo "TPM Installation Completed....\n"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  echo "TPM Installation Completed....\n"
 fi
 if [ -d "$HOME/.config/micro" ]; then
-	rm -rf $HOME/.config/micro/syntax
-	ln -sf $PWD/.package/micro/syntax $HOME/.config/micro/syntax
-	ln -sf $PWD/.package/micro/bindings.json $HOME/.config/micro/bindings.json
+  rm -rf $HOME/.config/micro/syntax
+  ln -sf $PWD/.package/micro/syntax $HOME/.config/micro/syntax
+  ln -sf $PWD/.package/micro/bindings.json $HOME/.config/micro/bindings.json
 else
-	mkdir ~/.config/micro
-	ln -sf $PWD/.package/micro/syntax $HOME/.config/micro/
-	ln -sf $PWD/.package/micro/bindings.json $HOME/.config/micro/bindings.json
+  mkdir ~/.config/micro
+  ln -sf $PWD/.package/micro/syntax $HOME/.config/micro/
+  ln -sf $PWD/.package/micro/bindings.json $HOME/.config/micro/bindings.json
+fi
+if [ -d "$HOME/.SpaceVim.d" ]; then
+  ln -sf $PWD/.package/SpaceVim.d/* $HOME/.SpaceVim.d
 fi
