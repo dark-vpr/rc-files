a04215a815cceada168161b41eec7b51e48f6c92
root <root@UNKNOWN.localdomain>
2022-06-11 11:58:24 +0530

    tpm bug fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-ln -sf $PWD/.package/.$HOME/.SpaceVim.dtmux.conf ~/.tmux.conf
+ln -sf $PWD/.package/.tmux.conf ~/.tmux.conf
 if [ -d $HOME/.tmux/plugins/tpm ]; then
   printf "TPM has already been Installed.....\n"
 else
@@ -11,11 +11,14 @@ if [ -d "$HOME/.config/micro" ]; then
   rm -rf $HOME/.config/micro/syntax
   ln -sf $PWD/.package/micro/syntax $HOME/.config/micro/syntax
   ln -sf $PWD/.package/micro/bindings.json $HOME/.config/micro/bindings.json
+  echo "MIcro Installation Completed....\n"
 else
   mkdir ~/.config/micro
   ln -sf $PWD/.package/micro/syntax $HOME/.config/micro/
   ln -sf $PWD/.package/micro/bindings.json $HOME/.config/micro/bindings.json
+  echo "Micro Installation Completed....\n"
 fi
 if [ -d "$HOME/.SpaceVim.d" ]; then
   ln -sf $PWD/.package/SpaceVim.d/* $HOME/.SpaceVim.d
+  echo "SpaceVim Installation Completed....\n"
 fi
